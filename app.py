eaf8dfda694e8a1467b08f542fd1a168ce5e3a90
Paul Edward Bennett <paul.e.bennett@icloud.com>
2019-01-19 19:44:00 +0000

    commit changes for bootstrap template

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello Flask'
+def base():
+    return render_template('base.html') 
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
-        
